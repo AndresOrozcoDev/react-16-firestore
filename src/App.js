cd124eeab6ad5f28ed0d8bef5f39146be95866bb
Andres Orozco <andres.orozco.dev@gmail.com>
2024-12-02 18:28:58 -0500

    pretty code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import './App.css';
 import './Variables.css';
-
-
 import Home from './pages/home/Home';
 import Add from './pages/add/Add';
 
